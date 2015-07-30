e33950c82dec71e4c24bce296aabf26411f4c276
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2015-07-30 12:23:40 +0200

    hack to work around sites not supporting caching

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var favicon = fs.readFileSync('favicon.ico');
 var port = process.env.PORT || 8080;
 
 var copyHeaders = ['Date', 'Last-Modified', 'Expires', 'Cache-Control', 'Pragma', 'Content-Length', 'Content-Type'];
+var copyClientHeaders = ['If-Modified-Since'];
 
 var server = http.createServer(function (req, res) {
 	var d = domain.create();
@@ -46,21 +47,49 @@ function handler(req, res) {
 			res.writeHead(200);
 			res.write(favicon);
 			res.end();
+      break;
 		default:
 			if (req.url.indexOf('vivastreet') > -1){
 				res.end('tempbanned');
 			} else {
 			try {
 				res.setTimeout(25000);
-				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader('Access-Control-Allow-Credentials', false);
-				res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 				res.setHeader('Expires', new Date(Date.now() + 86400000).toUTCString()); // one day in the future
-				var r = request(req.url.slice(1), {encoding: null});
+        if (req.headers['If-Modified-Since'.toLowerCase()]) {
+          console.log("If-Modified-Since", req.headers['if-modified-since']);
+          res.writeHead(304);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Access-Control-Allow-Credentials', false);
+          res.setHeader('Access-Control-Allow-Headers', 'Content-Type,If-None-Match,If-Modified-Since,' + copyHeaders.join());
+          res.setHeader('Last-Modified', req.headers['if-modified-since']);
+          res.end();
+          break;
+        }
+        var headers = {
+          encoding: null
+        };
+        for (var i=0; i < copyClientHeaders.length; i++) {
+          var clientHeader = req.headers[copyClientHeaders[i].toLowerCase()];
+          if (clientHeader) {
+            headers[copyHeaders[i]] = clientHeader;
+          }
+        }
+        var options = {
+          url: req.url.slice(1),
+          headers: headers
+        };
+				var r = request(options);
 				r.pipefilter = function(response, dest) {
 					for (var header in response.headers) {
 						dest.removeHeader(header);
 					}
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Access-Control-Allow-Credentials', false);
+          res.setHeader('Access-Control-Allow-Headers', 'Content-Type,If-None-Match,If-Modified-Since,' + copyHeaders.join());
+          var lastModified = response.headers['date'];
+          if (lastModified) {
+            res.setHeader('Last-Modified', lastModified);
+          }
 					for (var i=0; i<copyHeaders.length; i++) {
 						var responseHeader = response.headers[copyHeaders[i].toLowerCase()];
 						if (responseHeader) {
